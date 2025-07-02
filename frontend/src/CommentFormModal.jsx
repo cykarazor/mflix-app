6c0440bf43b6cbe3f090e7dc18d4036176fe8a69
Shawn Mohammed <shawn_mohammed@hotmail.com>
2025-07-02 13:23:09 -0400

    Added Suername and Email to CommentFtormModal

--- frontend/src/CommentFormModal.jsx
+++ frontend/src/CommentFormModal.jsx
@@ -25,6 +25,8 @@ export default function CommentFormModal({ open, onClose, movieId, token, onComm
         {
           movie_id: movieId,
           text,
+          name: user.name,
+          email: user.email,
         },
         {
           headers: { Authorization: `Bearer ${token}` },
